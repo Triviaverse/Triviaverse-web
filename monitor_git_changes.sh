8d2321473d44ed9deee5c664f3bfe0ec0773d3ac
Hazel <37238787+TNTTheLagger@users.noreply.github.com>
2025-01-30 11:23:01 +0100

    Made monitor_git_changes.sh output to tty1

--- monitor_git_changes.sh
+++ monitor_git_changes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+exec > /dev/tty1 2>&1
+
 # Monitor the repository for changes (modifications, creation, deletion of files)
 while inotifywait -r -e modify,create,delete ~/Triviaverse-web; do
     echo "Changes detected, pulling from Git..."
