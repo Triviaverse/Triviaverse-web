a75484144b3ae23eedd87f1e0bc7a3f3b3ec711d
FeketeAbel <77393377+FeketeAbel@users.noreply.github.com>
2025-02-05 21:08:57 +0100

    Add files via upload

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,27 @@
+<template>
+		<div id="pre-start">
+			<select id="problem-set" style="display: none;">
+			</select>
+			<button id="start">Bejelentkezés</button>
+		</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.dashboard-container {
+  min-height: 100vh;
+  background-color: #f8f9fa;
+}
+.profile-icon {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+</style>
